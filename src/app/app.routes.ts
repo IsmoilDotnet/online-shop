6e24cb0b2c77cb958128d8a20266042b68c6b9c2
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-04-04 08:53:08 +0500

    my first angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { HomeComponent } from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"Navbar",title:"navbar",component:NavbarComponent},
+    {path:"home", title:"home", component:HomeComponent},
+    {path:"footer", title:"footer", component:FooterComponent},
+    {path:"**",title:"NotFound",component:NotFoundComponent}
+];
